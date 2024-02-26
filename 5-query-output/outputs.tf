b75483b5648b73c0dce79459d476515c85a3c59b
Diego Fleitas <diego.fleitas68@gmail.com>
2024-02-26 12:04:43 -0300

    query output vars

---
+++ 5-query-output/outputs.tf
@@ -0,0 +1,11 @@
+# NOTE: we are using dynamic variables here, the ones at variables.tf were hardcoded
+# applying the changes will show the variables values on the terminal
+output "instance_id" {
+  description = "ID of the EC2 instance"
+  value       = aws_instance.app_server.id
+}
+
+output "instance_public_ip" {
+  description = "Public IP address of the EC2 instance"
+  value       = aws_instance.app_server.public_ip
+}
