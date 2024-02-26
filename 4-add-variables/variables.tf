ef2d36f015d1904fdce1c16d594da1f30598ed3a
Diego Fleitas <diego.fleitas68@gmail.com>
2024-02-26 11:41:21 -0300

    add variables

---
+++ 4-add-variables/variables.tf
@@ -0,0 +1,5 @@
+variable "instance_name" { # instance_name variable block
+  description = "Value of the Name tag for the EC2 instance"
+  type        = string
+  default     = "ExampleAppServerInstance"
+}
